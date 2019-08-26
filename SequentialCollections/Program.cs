fdf9b0d4bdf8a1430baa466a8807c16730aabcc8
samufromhungary <blzkproblem@gmail.com>
2019-08-26 09:38:13 +0200

    Add project files.

---
+++ SequentialCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("first");
+            queue.Enqueue("second");
+            queue.Enqueue("third");
+            queue.Enqueue("fourth");
+
+            while(queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From queue: {0}", obj);
+            }
+
+            Console.WriteLine("\r\n");
+
+            Stack stack = new Stack();
+            stack.Push("first");
+            stack.Push("second");
+            stack.Push("third");
+            stack.Push("fourth");
+
+            while(stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From stack: {0}", obj);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
